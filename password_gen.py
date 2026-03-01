15267faa92aabe55984d421d4e0b43a4e1e7c3ad
Acc1dEnT-er <erba80404@gmail.com>
2026-03-01 20:16:55 +0600

    Enhance password generator to create multiple passwords
    Added functionality to generate multiple passwords based on user input.

--- password_gen.py
+++ password_gen.py
@@ -2,8 +2,10 @@ import random
 import string
 
 length = int(input("Введите длину пароля: "))
+count = int(input("Сколько паролей сгенерировать: "))
 
 characters = string.ascii_letters + string.digits + string.punctuation
-password = "".join(random.choice(characters) for _ in range(length))
 
-print("Ваш пароль:", password)
+for i in range(count):
+    password = "".join(random.choice(characters) for _ in range(length))
+    print(f"Пароль {i+1}: {password}")
