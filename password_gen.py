48ae8c615d8e2edd0ac12bb3bc288974e5261c27
Acc1dEnT-er <erba80404@gmail.com>
2026-03-01 20:03:56 +0600

    Add files via upload

---
+++ password_gen.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+length = int(input("Введите длину пароля: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+password = "".join(random.choice(characters) for _ in range(length))
+
+print("Ваш пароль:", password)
